a227f76b187b6cea95bf0830a8cfa833e5a73538
kirito0san <jodat.ahmedsyed@gmail.com>
2024-10-04 17:05:45 +0300

    end of style product

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -4,12 +4,19 @@ import { ChangeDetectionStrategy } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common'; // Import CommonModule for ngFor and ngIf
-
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
-  imports: [MatCardModule, MatButtonModule, CommonModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    CommonModule,
+    FormsModule,
+  ],
   standalone: true,
 })
 export class ProductsComponent implements OnInit {
@@ -20,5 +27,15 @@ export class ProductsComponent implements OnInit {
       console.log(data);
       this.products = data;
     });
+  }
+  searchTerm: string = '';
+
+  get filteredItems(): any {
+    if (!this.searchTerm) {
+      return this.products;
+    }
+    return this.products.filter((item: any) =>
+      item.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 }
